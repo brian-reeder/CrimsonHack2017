52843cba319fc51a90bea71922511314e933c447
Derek Rogers <chiefisdgrpet@aol.com>
2017-03-25 13:15:08 -0500

    CompairArticles

---
+++ CompairArticles.py
@@ -0,0 +1,12 @@
+
+# calculate
+def calculatePercent(): #need to take in "article" and "articleToCompair"
+    print(50)
+
+# This fuction will compair every article to eachother and determine which articles are similar to eachother
+def compairArticles(array1, array2):
+    for article in array1:
+        for articleToCompair in array2:
+            percentSimilar = calculatePercent(article, articleToCompair)
+            if percentSimilar >= thresholdPercent:
+                array1(article) #need to add articleToCompair to article.SimilarArticles
